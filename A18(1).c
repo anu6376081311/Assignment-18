96b89fdc57b51c8accd3d4ade01295ad2b4b5d06
anumeena <111556950+anu6376081311@users.noreply.github.com>
2023-03-24 20:13:18 +0530

    Add files via upload

---
+++ A18(1).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void length( char a[]);
+int main()
+{
+    char a[20] ="BHOPAL";
+    
+    // printf("Enter string :");
+    // scanf("%s",str);
+    length(a[]);
+    return 0;
+
+}
+void length( char a[])
+{
+    int i;
+    for(i=0;i<=6;i++);
+    printf("length of the given string is %d",i);
+
+}
